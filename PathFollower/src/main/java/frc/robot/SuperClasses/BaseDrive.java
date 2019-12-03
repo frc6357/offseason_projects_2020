99e75f6bfbffd0fe7ae98aa92636bdad5c5be31a
David Pieper <david.pieper123@gmail.com>
2019-12-03 12:00:08 -0600

    Added documentation Added documentation for what is so far written in the path follower project. Need to go over with people how the documentation is written and why it is so powerful.

--- PathFollower/src/main/java/frc/robot/SuperClasses/BaseDrive.java
+++ PathFollower/src/main/java/frc/robot/SuperClasses/BaseDrive.java
@@ -3,11 +3,31 @@ package frc.robot.SuperClasses;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
+/**
+ * The base class for any 2 or 3 motor sided drive train that has multiple subclasses.
+ * @author David Pieper
+ */
 public abstract class BaseDrive
 {
     private SpeedController leftFront, rightFront, leftMiddle, rightMiddle, leftBack, rightBack;
     private SpeedControllerGroup leftMotors, rightMotors;
 
+    /**
+     * The constructor for the a standard two motor per side drive train
+     * @author David Pieper
+     * @param leftFront <br>
+     * - Type SpeedController <br>
+     * - Left front motor controller
+     * @param rightFront <br>
+     * - Type SpeedController <br>
+     * - Right front motor controlelr
+     * @param leftBack <br>
+     * - Type SpeedController <br>
+     * - Left back motor controller
+     * @param rightBack <br>
+     * - Type SpeedController <br>
+     * - Right back motor controller
+     */
     public BaseDrive (SpeedController leftFront, SpeedController rightFront, SpeedController leftBack, SpeedController rightBack)
     {
         this.leftFront = leftFront;
@@ -18,6 +38,28 @@ public abstract class BaseDrive
         this.rightMotors = new SpeedControllerGroup(this.rightFront, this.rightBack);
     }
 
+    /**
+     * The standard drive train constructor for a three motor per side drivetrain
+     * @author David Pieper
+     * @param leftFront <br>
+     * - Type SpeedController <br>
+     * - Left front motor controller
+     * @param leftMiddle <br>
+     * - Type SpeedController <br>
+     * - Left middle motor controller
+     * @param leftBack <br>
+     * - Type SpeedController <br>
+     * - Left back motor controller
+     * @param rightFront <br>
+     * - Type SpeedController <br>
+     * - Right front motor controller 
+     * @param rightMiddle <br>
+     * - Type SpeedController <br>
+     * - Right middle motor controller
+     * @param rightBack <br>
+     * - Type SpeedController <br>
+     * - Right rear motor controller
+     */
     public BaseDrive (SpeedController leftFront, SpeedController leftMiddle, SpeedController leftBack, SpeedController rightFront, SpeedController rightMiddle, SpeedController rightBack)
     {
         this.leftFront = leftFront;
@@ -30,13 +72,53 @@ public abstract class BaseDrive
         this.rightMotors = new SpeedControllerGroup(this.rightFront, this.rightMiddle, this.rightBack);
     }
 
+    /**
+     * Sets the speed for the left side of the drivetrain
+     * @author David Pieper
+     * @param input <br>
+     * - Type double <br>
+     * - The speed that the motor controller is going to be set to, 1 for full forwards and -1 for
+     * full back
+     */
     public void setLeftSpeed(double input)
     {
         leftMotors.set(input);
     }
 
+    /**
+     * Sets the speed for the right side of the drivetrain
+     * @author David Pieper
+     * @param input <br>
+     * - Type double <br>
+     * - The speed that the motor controller is going to be set to, 1 for full forwards and -1 for
+     * full back
+     */
     public void setRightSpeed(double input)
     {
         rightMotors.set(input);
+    }
+
+    /**
+     * Returns the current set speed for the left side of the drivetrain
+     * @author David Pieper
+     * @return
+     * - Type double <br>
+     * - Current set speed of motor controller, 1 for full forwards, -1 for full back
+     */
+    public double getLeftSpeed()
+    {
+        return leftMotors.get();
+    }
+
+    /**
+     * Returns the current set speed for the right side of the drivetrain
+     * @author David Pieper
+     * @return
+     * - Type double <br>
+     * - Current set speed of motor controller, 1 for full forwards, -1 for full back
+     */
+    public double getRightSpeed()
+    {
+        return rightMotors.get();
     }
 }
--- PathFollower/src/main/java/frc/robot/SuperClasses/BaseDriveHDrive.java
+++ PathFollower/src/main/java/frc/robot/SuperClasses/BaseDriveHDrive.java
@@ -3,11 +3,42 @@ package frc.robot.SuperClasses;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
-public class BaseDriveHDrive extends BaseDrive
+/**
+ * This code is meant to run an H-Drive in preperation for the 2020 offseason project as well as getting
+ * ready for if it's to be used in the future perhaps in the 2020 season. With this it needs some work
+ * as the main goal of this project is that we create a base class or set of base classes that can be
+ * used for multiple years. This can run an H-Drive, and has the pieces to be able to implement an
+ * H-Drive very easily in a PID controller.
+ * @author David Pieper
+ */
+public abstract class BaseDriveHDrive extends BaseDrive
 {
     SpeedController leftFront, leftMiddle, leftBack, rightFront, rightMiddle, rightBack, HDrive1, HDrive2;
     SpeedControllerGroup HDrive;
 
+    /**
+     * The H-Drive creation system that is required when there are only two motors for each side
+     * for the H-Drive as well as two motors for the H-Drive
+     * @author David Pieper
+     * @param leftFront <br>
+     * - Type SpeedController<br>
+     * - Front left motor controller
+     * @param rightFront <br>
+     * - Type SpeedController<br>
+     * - Front right motor controller
+     * @param leftBack <br>
+     * - Type SpeedController<br>
+     * - Back left motor controller
+     * @param rightBack <br>
+     * - Type SpeedController<br>
+     * - Back right motor controller
+     * @param HDrive1 <br>
+     * - Type SpeedController<br>
+     * - HDrive Motor Controller 1
+     * @param HDrive2 <br>
+     * - Type SpeedController<br>
+     * - HDrive Motor Controller 2
+     */
     public BaseDriveHDrive(SpeedController leftFront, SpeedController rightFront, SpeedController leftBack, SpeedController rightBack, SpeedController HDrive1, SpeedController HDrive2)
     {
         super(leftFront, leftBack, rightFront, rightBack);
@@ -16,6 +47,35 @@ public class BaseDriveHDrive extends BaseDrive
         HDrive = new SpeedControllerGroup(this.HDrive1, this.HDrive2);
     }
 
+    /**
+     * This constructor creates a system for an H-Drive utilizing two motors for the H-drive as
+     * well as 3 drive train motors per side.
+     * @author David Pieper
+     * @param leftFront <br>
+     * - Type SpeedController <br>
+     * - Left front motor controller
+     * @param leftMiddle <br>
+     * - Type SpeedController <br>
+     * - Left middle motor controller
+     * @param leftBack <br>
+     * - Type SpeedController <br>
+     * - Left back motor controller
+     * @param rightFront <br>
+     * - Type SpeedController <br>
+     * - Right front motor controller
+     * @param rightMiddle <br>
+     * - Type SpeedController <br>
+     * - Right middle motor controller
+     * @param rightBack <br>
+     * - Type SpeedController <br>
+     * - Right back motor controller
+     * @param HDrive1 <br>
+     * - Type SpeedController <br>
+     * - Motor controller for the first H-Drive motor
+     * @param HDrive2 <br>
+     * - Type SpeedController <br>
+     * - Motor controller for the second H-Drive motor
+     */
     public BaseDriveHDrive (SpeedController leftFront, SpeedController leftMiddle, SpeedController leftBack, SpeedController rightFront, SpeedController rightMiddle, SpeedController rightBack, SpeedController HDrive1, SpeedController HDrive2)
     {
         super(leftFront, leftMiddle, leftBack, rightFront, rightMiddle, rightBack);
@@ -24,8 +84,28 @@ public class BaseDriveHDrive extends BaseDrive
         HDrive = new SpeedControllerGroup(this.HDrive1, this.HDrive2);
     }
 
+    /**
+     * Sets the speed of the H-Drive with 1 being to the right and -1 being to the left
+     * @author David Pieper
+     * @param input <br>
+     * - Type double <br>
+     * - Is the speed of the H-Drive, with 1 accelerating to the right, and -1 accelerating to
+     * the left
+     */
     public void setHDriveSpeed (double input)
     {
         HDrive.set(input);
+    }
+
+    /**
+     * Returns the speed of the H-Drive
+     * @author David Pieper
+     * @return
+     * - Type double <br>
+     * - Is the set speed of the H-drive, with 1 moving to the right, and -1 moving to the left
+     */
+    public double getHDriveSpeed()
+    {
+        return HDrive.get();
     }
 }
